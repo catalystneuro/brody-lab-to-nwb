f64a99971b9ed0849e6c2214761883ad7ffabb51
Cody Baker <cbaker9@nd.edu>
2021-09-24 14:34:16 -0400

    added and debugged protocol info interface

--- brody_lab_to_nwb/interfaces/protocol_info/protocol_info_utils.py
+++ brody_lab_to_nwb/interfaces/protocol_info/protocol_info_utils.py
@@ -8,64 +8,64 @@ import pickle
 import seaborn as sns
 import scipy.io as spio
 
-# "Import from matlab & wrangle"
-# def load_nested_mat(filename):
-#     """
-#     This function should be called instead of direct scipy.io.loadmat
-#     as it cures the problem of not properly recovering python dictionaries
-#     from mat files. It calls the function check keys to cure all entries
-#     which are still mat-objects. From https://stackoverflow.com/questions
-#     /48970785/complex-matlab-struct-mat-file-read-by-python
-#     """
 
-#     def _check_vars(d):
-#         """
-#         Checks if entries in dictionary are mat-objects. If yes
-#         todict is called to change them to nested dictionaries
-#         """
-#         for key in d:
-#             if isinstance(d[key], spio.matlab.mio5_params.mat_struct):
-#                 d[key] = _todict(d[key])
-#             elif isinstance(d[key], np.ndarray):
-#                 d[key] = _toarray(d[key])
-#         return d
+def load_nested_mat(filename):
+    """
+    This function should be called instead of direct scipy.io.loadmat
+    as it cures the problem of not properly recovering python dictionaries
+    from mat files. It calls the function check keys to cure all entries
+    which are still mat-objects. From https://stackoverflow.com/questions
+    /48970785/complex-matlab-struct-mat-file-read-by-python
+    """
 
-#     def _todict(matobj):
-#         """
-#         A recursive function which constructs from matobjects nested dictionaries
-#         """
-#         d = {}
-#         for strg in matobj._fieldnames:
-#             elem = matobj.__dict__[strg]
-#             if isinstance(elem, spio.matlab.mio5_params.mat_struct):
-#                 d[strg] = _todict(elem)
-#             elif isinstance(elem, np.ndarray):
-#                 d[strg] = _toarray(elem)
-#             else:
-#                 d[strg] = elem
-#         return d
+    def _check_vars(d):
+        """
+        Checks if entries in dictionary are mat-objects. If yes
+        todict is called to change them to nested dictionaries
+        """
+        for key in d:
+            if isinstance(d[key], spio.matlab.mio5_params.mat_struct):
+                d[key] = _todict(d[key])
+            elif isinstance(d[key], np.ndarray):
+                d[key] = _toarray(d[key])
+        return d
 
-#     def _toarray(ndarray):
-#         """
-#         A recursive function which constructs ndarray from cellarrays
-#         (which are loaded as numpy ndarrays), recursing into the elements
-#         if they contain matobjects.
-#         """
-#         if ndarray.dtype != 'float64':
-#             elem_list = []
-#             for sub_elem in ndarray:
-#                 if isinstance(sub_elem, spio.matlab.mio5_params.mat_struct):
-#                     elem_list.append(_todict(sub_elem))
-#                 elif isinstance(sub_elem, np.ndarray):
-#                     elem_list.append(_toarray(sub_elem))
-#                 else:
-#                     elem_list.append(sub_elem)
-#             return np.array(elem_list)
-#         else:
-#             return ndarray
+    def _todict(matobj):
+        """
+        A recursive function which constructs from matobjects nested dictionaries
+        """
+        d = {}
+        for strg in matobj._fieldnames:
+            elem = matobj.__dict__[strg]
+            if isinstance(elem, spio.matlab.mio5_params.mat_struct):
+                d[strg] = _todict(elem)
+            elif isinstance(elem, np.ndarray):
+                d[strg] = _toarray(elem)
+            else:
+                d[strg] = elem
+        return d
 
-#     data = spio.loadmat(filename, struct_as_record=False, squeeze_me=True)
-#     return _check_vars(data)
+    def _toarray(ndarray):
+        """
+        A recursive function which constructs ndarray from cellarrays
+        (which are loaded as numpy ndarrays), recursing into the elements
+        if they contain matobjects.
+        """
+        if ndarray.dtype != 'float64':
+            elem_list = []
+            for sub_elem in ndarray:
+                if isinstance(sub_elem, spio.matlab.mio5_params.mat_struct):
+                    elem_list.append(_todict(sub_elem))
+                elif isinstance(sub_elem, np.ndarray):
+                    elem_list.append(_toarray(sub_elem))
+                else:
+                    elem_list.append(sub_elem)
+            return np.array(elem_list)
+        else:
+            return ndarray
+
+    data = spio.loadmat(filename, struct_as_record=False, squeeze_me=True)
+    return _check_vars(data)
 
 
 def load_and_wrangle_behavior(beh_path):
--- brody_lab_to_nwb/interfaces/protocol_info/protocolinfodatainterface.py
+++ brody_lab_to_nwb/interfaces/protocol_info/protocolinfodatainterface.py
@@ -1,11 +1,14 @@
 """Authors: Cody Baker and Jess Breda."""
 import numpy as np
+import pandas as pd
 
 from pynwb import NWBFile
 from nwb_conversion_tools.basedatainterface import BaseDataInterface
 from nwb_conversion_tools.utils.json_schema import FilePathType
 
 from .protocol_info_utils import load_behavior, make_beh_df
+
+DEFAULT_COLUMN_MAP = dict(a=2)
 
 
 class ProtocolInfoInterface(BaseDataInterface):
@@ -32,48 +35,23 @@ class ProtocolInfoInterface(BaseDataInterface):
         behavior_info = load_behavior(self.source_data["file_path"])
         self.behavior_df = make_beh_df(behavior_info)
 
-    def run_conversion(self, nwbfile: NWBFile, metadata: dict, column_map: dict, column_descriptions: dict):
+    def run_conversion(self, nwbfile: NWBFile, metadata: dict):
         """
         Convert the values in the behavioral dataframe object to the NWBFile Trials table.
 
-        Parameters
-        ----------
-        nwbfile : NWBFile
-            DESCRIPTION.
-        metadata : dict
-            DESCRIPTION.
-        column_map : dict
-            A dictionary mapping the column names in the dataframe to intended column names in the NWBFile Trials table.
-            We encourage the user to remap any columns that represent timed events by adding _time or _times
-            (for multiple) to the end of the NWBFile name.
-
-            E.g.,
-                column_map = dict(hit_hist=hit_history, c_poke=c_poke_time, ...)
-
-            Likewise, if there are any binary conditions, we encourage manipulating the underlying data to be boolean,
-            and to change the name of the column to is_* where * is the intended name.
-
-            E.g., if
-                self.behavior_df["laser_on"][:] = [1 0 0 1 ...]
-            then set
-                self.behavior_df["laser_on"][:] = [True False False True ...]
-            and
-                column_map = dict(laser_on=is_laser_on, ...)
-        column_descriptions : dict
-            A dictionary pairing the NWBFile column names with string text describing their context in the experiment.
+        Maps the column names and descriptions via the column_mapping.csv located in the same folder as this interface.
+        To add new columns to extract from protocol_info.mat, be sure to add the naming details to the .csv file.
         """
-        assert len(column_map) == len(column_descriptions), (
-            f"The length of the column_map ({len(column_map)}) does not match the length of "
-            f"column_descriptions ({len(column_descriptions)})!"
-        )
+        column_mapping = pd.read_csv("column_mapping.csv", keep_default_na=False)
 
-        for nwb_name, description in column_descriptions.items():
-            nwbfile.add_trial_column(name=nwb_name, description=description)
+        for nwb_name, nwb_description in zip(column_mapping["nwb_name"], column_mapping["nwb_description"]):
+            nwbfile.add_trial_column(name=nwb_name, description=nwb_description)
 
         n_trials = self.behavior_df.shape[0]
         for k in range(n_trials):
             trial_kwargs = dict()
-            for mat_name, nwb_name in column_map.items():
+            for mat_name, nwb_name in zip(column_mapping["mat_name"], column_mapping["nwb_name"]):
                 trial_kwargs.update({nwb_name: self.behavior_df[mat_name][k]})
-            all_times = trial_kwargs.values()
+            all_times = [v for k, v in trial_kwargs.items() if "time" in k]
             trial_kwargs.update(start_time=np.min(all_times), stop_time=np.max(all_times))
+            nwbfile.add_trial(**trial_kwargs)
